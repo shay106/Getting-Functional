7f1bef3c4c00e2335602cdd83714f593c62b5faa
Jonathan Corley <jcorley@noscihigh.org>
2021-03-29 15:55:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//DO NOT CHANGE CODE HERE
+var customers = require('./customers.json');
+//DO NOT CHANGE CODE HERE
+
+
+
+
+/*
+1. Male Count
+
+Write a function that counts the number of males in 
+the customer object and return the number of males 
+in the customers list.
+
+
+**HINT: Look at the data in the customer.json file. 
+
+* What kind of loop should you use to iterate over 
+the customers array?
+
+* What data type occupies each element of the 
+customers array? What type of loop should be used 
+then?
+
+
+*/
+
+function maleCount(array){
+
+};
+
+
+/*
+2. Female Count
+
+Write a function that counts the number of females in 
+the customer object and return the number of 
+females in the customers list.
+
+
+**HINT: Look at the data in the customer.json file. 
+
+* What kind of loop should you use to iterate over 
+the customers array?
+
+* What data type occupies each element of the 
+customers array? What type of loop should be used 
+then?
+
+*/
+
+function maleCount(array){
+
+};
